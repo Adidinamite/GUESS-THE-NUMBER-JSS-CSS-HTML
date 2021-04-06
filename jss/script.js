c66381abf0cb922766ab7e097dcc0a4335bf3262
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-04-06 15:21:31 +0300

    Update script.js

--- jss/script.js
+++ jss/script.js
@@ -1,30 +1,55 @@
 let computerGuess;
 let userGuesses = [];
+let attempts = 0;
+let maximum_attempts = 0;
 
 function init(){
     computerGuess = Math.floor(Math.random() * 100 + 1);
     //console.log(computerGuess);
     document.getElementById("newGameButton").style.display="none";
     document.getElementById("gameArea").style.display="none"; // none/ block
+    document.getElementById("gameOver").style.display ="none";
+    attempts = 0;
+    maximum_attempts=0;
+    document.getElementById("inputBox").value = "";
+    document.getElementById("guesses").innerHTML = 0;
 }
 
 function startGameView(){
     document.getElementById("welcomeScreen").style.display = "none";
     document.getElementById("gameArea").style.display = "block";
 }
+function restart(){
+    init();
+    document.getElementById("welcomeScreen").style.display = "block";
+}
 function easyMode()
 {
     startGameView();
+    maximum_attempts=9;
+    document.getElementById("attempts").innerHTML = maximum_attempts + 1;
 }
 function hardMode()
 {
     startGameView();
+    maximum_attempts=4;
+    document.getElementById("attempts").innerHTML = maximum_attempts + 1;
 }
 function compareGuess(){
     const userGuess = Number(document.getElementById("inputBox").value)
-    userGuesses.push(" " + userGuess);
-    document.getElementById("guesses").innerHTML = userGuesses;
+    if(attempts<=maximum_attempts)
+    {
+        userGuesses.push(" " + userGuess);
+        document.getElementById("guesses").innerHTML = userGuesses;
+        document.getElementById("attempts").innerHTML = maximum_attempts - attempts;
+    }
+    else if(attempts >=maximum_attempts)
+    {
+        document.getElementById("gameOver").style.display ="block";
+        document.getElementById("gameArea").style.display = "none";
+    }
 
+    attempts++;
     if(userGuess > computerGuess)
     {
         document.getElementById("textOutput").innerHTML = "Your guess is higher than weed:(";
